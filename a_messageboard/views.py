18ed11d090228d2baf813b748ba1bfd69267b68f
psatomas <psatomas@gmail.com>
2025-12-04 21:03:27 -0300

    feat: add messageboard_home view and index template

--- a_messageboard/views.py
+++ a_messageboard/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required 
+def messageboard_home(request):
+    return render(request, 'a_messageboard/index.html')
