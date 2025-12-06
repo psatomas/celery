8b82889cab54816db0334a701003a3e9b2509da4
psatomas <psatomas@gmail.com>
2025-12-06 17:29:28 -0300

    fix: remove broken subscribe block that caused template error

--- a_core/urls.py
+++ a_core/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('', include('a_home.urls')),
     path('profile/', include('a_users.urls')),
     path('@<username>/', profile_view, name="profile"),
+    path('messageboard/', include('a_messageboard.urls')),
 ]
 
 # Only used when DEBUG=True, whitenoise can serve files when DEBUG=False
--- a_messageboard/views.py
+++ a_messageboard/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 @login_required 
-def messageboard_home(request):
+def messageboard_view(request):
     return render(request, 'a_messageboard/index.html')
