2beffbde7bcde8e5918ecf2c49afc758a5ab94bf
psatomas <psatomas@gmail.com>
2025-12-07 19:10:33 -0300

    feat: add subscribers ManyToManyField to MessageBoard

--- a_messageboard/models.py
+++ a_messageboard/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User    
+class MessageBoard(models.Model):
+    subscribers = models.ManyToManyField(User, related_name='messageboard', blank=True)
 
-# Create your models here.
+    def __str__(self):
+        return str(self.id)
+    
