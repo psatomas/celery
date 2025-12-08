574edb6c4c6c4d7b925a960cc49cd0acb7f44527
psatomas <psatomas@gmail.com>
2025-12-08 17:25:56 -0300

    feat(views): load default MessageBoard instance into context

--- a_messageboard/views.py
+++ a_messageboard/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from .models import *
 
 @login_required 
 def messageboard_view(request):
-    return render(request, 'a_messageboard/index.html')
+    messageboard = get_object_or_404(MessageBoard, id=1)
+
+    context = {
+        'messageboard': messageboard,
+    }
+    return render(request, 'a_messageboard/index.html', context)
